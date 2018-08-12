2b973b4106b66dec4af9d7bbf629dd23ae2d2c14
Fernando Suárez <fsuarezj@gmail.com>
2018-08-12 21:31:39 +0200

    Added channel location, advanced stream info and tests

--- bitalino_lsl/bitalinoLSL.py
+++ bitalino_lsl/bitalinoLSL.py
@@ -13,30 +13,66 @@ def list_bitalino():
 
 class ExceptionCode():
     WRONG_CHANNEL = "The specified channel/s is invalid."
+    WRONG_CHANNEL_LOCATION = "The specified channel/s is not complied with 10-20 system in bipolar configuration (e.g. F7-F3)."
 
 class BitalinoLSL(object):
+    _eeg_positions = [       "Fp1",          "Fp2",
+                    "F7",   "F3",   "Fz",   "F4",   "F8",
+            "A1",   "T3",   "C3",   "Cz",   "C4",   "T4",   "A2",
+                    "T5",   "P3",   "Pz",   "P4",   "T6",
+                            "O1",           "O2"]
     _sampling_rate = 1000
-    _eeg_channels = []
+    _eeg_channels = dict()
 
     def __init__(self, mac_address, timeout = None):
         print("Connecting to BITalino with MAC {mac_address}")
         self.bitalino = bitalino.BITalino(mac_address, timeout)
 
     def create_lsl_EEG(self, channels):
-
-        if type(channels) != list:
-            if not channels in range(6):
-                raise Exception(ExceptionCode.WRONG_CHANNEL)
-            eeg_channels = [channels]
-        else:
+        aux = dict()
+        if type(channels) == dict:
+            for i in list(channels.keys()):
+                if not i in range(6):
+                    raise Exception(ExceptionCode.WRONG_CHANNEL)
+                if type(channels[i]) != str:
+                    raise Exception(ExceptionCode.WRONG_CHANNEL_LOCATION)
+                else:
+                    ch_loc = channels[i].split('-')
+                    if len(ch_loc) != 2:
+                        raise Exception(ExceptionCode.WRONG_CHANNEL_LOCATION)
+                    for j in ch_loc:
+                        if j not in self._eeg_positions:
+                            raise Exception(ExceptionCode.WRONG_CHANNEL_LOCATION)
+            self._eeg_channels = channels
+        elif type(channels) == list:
             for i in channels:
                 if not i in range(6):
                     raise Exception(ExceptionCode.WRONG_CHANNEL)
-            eeg_channels = channels
+                else:
+                    aux[i] = ""
+            self._eeg_channels = aux
+        else:
+            if not channels in range(6):
+                raise Exception(ExceptionCode.WRONG_CHANNEL)
+            aux[channels] = ""
+            self._eeg_channels = aux
 
         print("Creating LSL")
         # ToDo: Replace the name for a lookup in the devices dict
-        self._info_eeg = StreamInfo("BITalino", "EEG", len(eeg_channels), self._sampling_rate, 'float32', "BITalino_5160_EEG")
+        self._info_eeg = StreamInfo("BITalino", "EEG", len(list(self._eeg_channels.keys())), self._sampling_rate, 'float32', "BITalino_5160_EEG")
+        self._info_eeg.desc().append_child_value("manufacturer", "BITalino")
+        chns = self._info_eeg.desc().append_child("channels")
+        for i in self._eeg_channels.keys():
+            ch = chns.append_child("channel")
+            ch.append_child_value("label", str(self._eeg_channels[i]))
+            ch.append_child_value("unit", "microvolts")
+            ch.append_child_value("type", "EEG")
+
+    def get_active_channels(self):
+        return list(self._eeg_channels.keys())
+
+    def locate_bipolar_channels(self):
+        print("Hey")
 
     def stream(self):
         print("Start streaming")
--- tests/test_bitalino_streamInfo.py
+++ tests/test_bitalino_streamInfo.py
@@ -2,6 +2,13 @@ import pytest
 import re
 from context import bitalino_lsl
 
+## Marks are:
+## dev_test: test used for developing purposes
+## exc_test: test not launching exceptions
+## int_test: test using an integer as input for the channels
+## list_test: test using a list as input for the channels
+## dict_test: test using a dictionary as input for the channels
+
 @pytest.fixture(scope="session")
 def data():
     pytest.mac_address = "20:17:11:20:51:60"
@@ -10,54 +17,211 @@ def data():
 def connect_my_bitalino(data):
     pytest.device = bitalino_lsl.BitalinoLSL(pytest.mac_address)
 
-@pytest.mark.no_exc
-def test_create_StreamInfo_channel(data):
+@pytest.mark.dev_test
+def test_developing_test(data, capsys):
+    pytest.device.create_lsl_EEG([4,2])
+    with capsys.disabled():
+        print(pytest.device._info_eeg.as_xml())
+        ch = pytest.device._info_eeg.desc().child("channels").child("channel")
+        for i in range(pytest.device._info_eeg.channel_count()):
+            print(ch.child_value("label"))
+            print(ch.child_value("unit"))
+            print(ch.child_value("type"))
+            ch = ch.next_sibling()
+
+@pytest.mark.int_test
+def test_create_lsl_EEG_channel_int(data, capsys):
     pytest.device.create_lsl_EEG(2)
     assert pytest.device._info_eeg.__class__.__name__ == "StreamInfo"
     assert pytest.device._info_eeg.name() == "BITalino"
     assert pytest.device._info_eeg.type() == "EEG"
     assert pytest.device._info_eeg.channel_count() == 1
+    #with capsys.disabled():
+    #    print(pytest.device._eeg_channels)
     assert pytest.device._info_eeg.nominal_srate() == 1000
+    assert pytest.device._info_eeg.desc().child_value("manufacturer") == "BITalino"
+    ch = pytest.device._info_eeg.desc().child("channels").child("channel")
+    for i in pytest.device.get_active_channels():
+        assert ch.child_value("label") == ""
+        assert ch.child_value("unit") == "microvolts"
+        assert ch.child_value("type") == "EEG"
+        ch = ch.next_sibling()
 
-@pytest.mark.no_exc
-def test_create_StreamInfo_channels(data):
+@pytest.mark.list_test
+def test_create_lsl_EEG_channel_list(data, capsys):
+    pytest.device.create_lsl_EEG([5])
+    assert pytest.device._info_eeg.__class__.__name__ == "StreamInfo"
+    assert pytest.device._info_eeg.name() == "BITalino"
+    assert pytest.device._info_eeg.type() == "EEG"
+    assert pytest.device._info_eeg.channel_count() == 1
+    assert pytest.device._info_eeg.nominal_srate() == 1000
+    assert pytest.device._info_eeg.desc().child_value("manufacturer") == "BITalino"
+    ch = pytest.device._info_eeg.desc().child("channels").child("channel")
+    for i in pytest.device.get_active_channels():
+        assert ch.child_value("label") == ""
+        assert ch.child_value("unit") == "microvolts"
+        assert ch.child_value("type") == "EEG"
+        ch = ch.next_sibling()
+
+@pytest.mark.dict_test
+def test_create_lsl_EEG_channel_dict(data):
+    pytest.device.create_lsl_EEG({0: 'Pz-P4'})
+    assert pytest.device._info_eeg.__class__.__name__ == "StreamInfo"
+    assert pytest.device._info_eeg.name() == "BITalino"
+    assert pytest.device._info_eeg.type() == "EEG"
+    assert pytest.device._info_eeg.channel_count() == 1
+    assert pytest.device._info_eeg.nominal_srate() == 1000
+    assert pytest.device._info_eeg.desc().child_value("manufacturer") == "BITalino"
+    ch = pytest.device._info_eeg.desc().child("channels").child("channel")
+    for i in pytest.device.get_active_channels():
+        assert ch.child_value("label") == str(pytest.device._eeg_channels[i])
+        assert ch.child_value("unit") == "microvolts"
+        assert ch.child_value("type") == "EEG"
+        ch = ch.next_sibling()
+
+@pytest.mark.list_test
+def test_create_lsl_EEG_channels_list(data):
     pytest.device.create_lsl_EEG([0,1])
     assert pytest.device._info_eeg.__class__.__name__ == "StreamInfo"
     assert pytest.device._info_eeg.name() == "BITalino"
     assert pytest.device._info_eeg.type() == "EEG"
     assert pytest.device._info_eeg.channel_count() == 2
     assert pytest.device._info_eeg.nominal_srate() == 1000
+    assert pytest.device._info_eeg.desc().child_value("manufacturer") == "BITalino"
+    ch = pytest.device._info_eeg.desc().child("channels").child("channel")
+    for i in pytest.device.get_active_channels():
+        assert ch.child_value("label") == ""
+        assert ch.child_value("unit") == "microvolts"
+        assert ch.child_value("type") == "EEG"
+        ch = ch.next_sibling()
 
-@pytest.mark.no_exc
-def test_create_StreamInfo_channels2(data):
+@pytest.mark.list_test
+def test_create_lsl_EEG_channels_list2(data):
     pytest.device.create_lsl_EEG([5,0,1])
     assert pytest.device._info_eeg.__class__.__name__ == "StreamInfo"
     assert pytest.device._info_eeg.name() == "BITalino"
     assert pytest.device._info_eeg.type() == "EEG"
     assert pytest.device._info_eeg.channel_count() == 3
     assert pytest.device._info_eeg.nominal_srate() == 1000
+    assert pytest.device._info_eeg.desc().child_value("manufacturer") == "BITalino"
+    ch = pytest.device._info_eeg.desc().child("channels").child("channel")
+    for i in pytest.device.get_active_channels():
+        assert ch.child_value("label") == ""
+        assert ch.child_value("unit") == "microvolts"
+        assert ch.child_value("type") == "EEG"
+        ch = ch.next_sibling()
 
-def test_create_StreamInfo_exc1():
+@pytest.mark.dict_test
+def test_create_lsl_EEG_channels_dict(data):
+    pytest.device.create_lsl_EEG({3: 'F3-F7', 1: 'C4-T4'})
+    assert pytest.device._info_eeg.__class__.__name__ == "StreamInfo"
+    assert pytest.device._info_eeg.name() == "BITalino"
+    assert pytest.device._info_eeg.type() == "EEG"
+    assert pytest.device._info_eeg.channel_count() == 2
+    assert pytest.device._info_eeg.nominal_srate() == 1000
+    assert pytest.device._info_eeg.desc().child_value("manufacturer") == "BITalino"
+    ch = pytest.device._info_eeg.desc().child("channels").child("channel")
+    for i in pytest.device.get_active_channels():
+        assert ch.child_value("label") == str(pytest.device._eeg_channels[i])
+        assert ch.child_value("unit") == "microvolts"
+        assert ch.child_value("type") == "EEG"
+        ch = ch.next_sibling()
+
+@pytest.mark.dict_test
+def test_create_lsl_EEG_channels_dict2(data):
+    pytest.device.create_lsl_EEG({3: 'F3-F7', 1: 'C4-T4', 2: 'Fz-F4', 5: 'O1-O2', 0: 'Fp2-Fp1', 4: 'Pz-P3'})
+    assert pytest.device._info_eeg.__class__.__name__ == "StreamInfo"
+    assert pytest.device._info_eeg.name() == "BITalino"
+    assert pytest.device._info_eeg.type() == "EEG"
+    assert pytest.device._info_eeg.channel_count() == 6
+    assert pytest.device._info_eeg.nominal_srate() == 1000
+    assert pytest.device._info_eeg.desc().child_value("manufacturer") == "BITalino"
+    ch = pytest.device._info_eeg.desc().child("channels").child("channel")
+    for i in pytest.device.get_active_channels():
+        assert ch.child_value("label") == str(pytest.device._eeg_channels[i])
+        assert ch.child_value("unit") == "microvolts"
+        assert ch.child_value("type") == "EEG"
+        ch = ch.next_sibling()
+
+@pytest.mark.exc_test
+@pytest.mark.int_test
+def test_create_lsl_EEG_wrong_channel():
     with pytest.raises(Exception) as excinfo:
         pytest.device.create_lsl_EEG(6)
     assert "The specified channel/s is invalid." in str(excinfo.value)
 
-def test_create_StreamInfo_exc2():
-    with pytest.raises(Exception) as excinfo:
-        pytest.device.create_lsl_EEG([0,1,2,3,4,6])
-    assert "The specified channel/s is invalid." in str(excinfo.value)
-
-def test_create_StreamInfo_exc3():
+@pytest.mark.exc_test
+def test_create_lsl_EEG_wrong_channel2():
     with pytest.raises(Exception) as excinfo:
         pytest.device.create_lsl_EEG("a")
     assert "The specified channel/s is invalid." in str(excinfo.value)
 
-def test_create_StreamInfo_exc4():
+@pytest.mark.exc_test
+@pytest.mark.list_test
+def test_create_lsl_EEG_wrong_list():
+    with pytest.raises(Exception) as excinfo:
+        pytest.device.create_lsl_EEG([0,1,2,3,4,6])
+    assert "The specified channel/s is invalid." in str(excinfo.value)
+
+@pytest.mark.exc_test
+@pytest.mark.list_test
+def test_create_lsl_EEG_wrong_list2():
     with pytest.raises(Exception) as excinfo:
         pytest.device.create_lsl_EEG(["3", "2", 1])
     assert "The specified channel/s is invalid." in str(excinfo.value)
 
-def test_create_StreamInfo_exc5():
+@pytest.mark.exc_test
+@pytest.mark.list_test
+def test_create_lsl_EEG_wrong_list3():
     with pytest.raises(Exception) as excinfo:
         pytest.device.create_lsl_EEG([0,"a", "b", 1])
     assert "The specified channel/s is invalid." in str(excinfo.value)
+
+@pytest.mark.exc_test
+@pytest.mark.dict_test
+def test_create_lsl_EEG_wrong_dict_channel(data):
+    with pytest.raises(Exception) as excinfo:
+        pytest.device.create_lsl_EEG({3: 'F3-F7', 6: 'C4-T4'})
+    assert "The specified channel/s is invalid." in str(excinfo.value)
+
+@pytest.mark.exc_test
+@pytest.mark.dict_test
+def test_create_lsl_EEG_wrong_dict_channel2(data):
+    with pytest.raises(Exception) as excinfo:
+        pytest.device.create_lsl_EEG({9: 'Fp2-Fp1'})
+    assert "The specified channel/s is invalid." in str(excinfo.value)
+
+@pytest.mark.exc_test
+@pytest.mark.dict_test
+def test_create_lsl_EEG_wrong_dict_location(data):
+    with pytest.raises(Exception) as excinfo:
+        pytest.device.create_lsl_EEG({3: 'F3-F7', 1: 'C4-T7', 2: 'Fz-F4', 5: 'O1-O2', 0: 'Fp2-Fp1', 4: 'Pz-P3'})
+    assert "The specified channel/s is not complied with 10-20 system in bipolar configuration (e.g. F7-F3)." in str(excinfo.value)
+
+@pytest.mark.exc_test
+@pytest.mark.dict_test
+def test_create_lsl_EEG_wrong_dict_location2(data):
+    with pytest.raises(Exception) as excinfo:
+        pytest.device.create_lsl_EEG({3: 'F3-F7', 5: 'O1-O2', 0: 'Fp2 Fp1', 4: 'Pz-P3'})
+    assert "The specified channel/s is not complied with 10-20 system in bipolar configuration (e.g. F7-F3)." in str(excinfo.value)
+
+@pytest.mark.exc_test
+@pytest.mark.dict_test
+def test_create_lsl_EEG_wrong_dict_location3(data):
+    with pytest.raises(Exception) as excinfo:
+        pytest.device.create_lsl_EEG({3: 'F3-F7', 1: 'C4-T4', 2: 'FZ-F4'})
+    assert "The specified channel/s is not complied with 10-20 system in bipolar configuration (e.g. F7-F3)." in str(excinfo.value)
+
+@pytest.mark.exc_test
+@pytest.mark.dict_test
+def test_create_lsl_EEG_wrong_dict_location4(data):
+    with pytest.raises(Exception) as excinfo:
+        pytest.device.create_lsl_EEG({3: 'F3-F7', 1: 'C4-T4-O1'})
+    assert "The specified channel/s is not complied with 10-20 system in bipolar configuration (e.g. F7-F3)." in str(excinfo.value)
+
+@pytest.mark.exc_test
+@pytest.mark.dict_test
+def test_create_lsl_EEG_wrong_dict_location4(data):
+    with pytest.raises(Exception) as excinfo:
+        pytest.device.create_lsl_EEG({3: 'F3'})
+    assert "The specified channel/s is not complied with 10-20 system in bipolar configuration (e.g. F7-F3)." in str(excinfo.value)
