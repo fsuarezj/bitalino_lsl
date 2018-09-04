d109dd06b9d5cb35e3eb05ec161d30bfa2bc4fe9
Fernando Suárez <fsuarezj@gmail.com>
2018-09-05 00:16:09 +0200

    Tests restructured

--- tests/mocking/test_bitalino_stream_mock.py
+++ tests/mocking/test_bitalino_stream_mock.py
@@ -1,3 +1,4 @@
+import bitalino
 import pytest
 import re
 from context import bitalino_lsl
@@ -15,29 +16,32 @@ import time
 def data():
     pytest.mac_address = "20:17:11:20:51:60"
 
-@pytest.fixture(scope="module", autouse=True)
-def connect_my_bitalino(data):
+#@pytest.fixture(scope="module", autouse=True)
+#def connect_my_bitalino(data, mocker):
+#    mocker.patch.object(bitalino_lsl, 'bitalino.BITalino')
+#    pytest.device = bitalino_lsl.BitalinoLSL(pytest.mac_address)
+
+#@pytest.mark.dev_test
+#def test_developing_test(capsys):
+    #pytest.device.create_lsl_EEG({0: 'Fp1-Fp2', 1: 'T3-T5'})
+    #sampling_rate = pytest.device.get_sampling_rate()
+    #with capsys.disabled():
+        #pytest.device.start()
+        #streams = resolve_stream('type', 'EEG')
+        #inlet = StreamInlet(streams[0])
+        #datitos = inlet.pull_sample() ### Here is the problem!!!
+        #old_sample, old_timestamp = (datitos[0], datitos[1])
+        #time.sleep(2)
+        #pytest.device.stop()
+
+@pytest.mark.mock
+def test_stream(data, capsys, mocker):
+    mocker.patch.object(bitalino, 'BITalino')
     pytest.device = bitalino_lsl.BitalinoLSL(pytest.mac_address)
-
-@pytest.mark.dev_test
-def test_developing_test(capsys):
-    pytest.device.create_lsl_EEG({0: 'Fp1-Fp2', 1: 'T3-T5'})
-    sampling_rate = pytest.device.get_sampling_rate()
-    with capsys.disabled():
-        pytest.device.start()
-        streams = resolve_stream('type', 'EEG')
-        inlet = StreamInlet(streams[0])
-        datitos = inlet.pull_sample() ### Here is the problem!!!
-        old_sample, old_timestamp = (datitos[0], datitos[1])
-        time.sleep(2)
-        pytest.device.stop()
-
-# TODO: Quitar Mock
-@pytest.mark.integra
-def test_stream(capsys, mocker):
     channels = {0: 'Fp1-Fp2', 1: 'T3-T5'}
     pytest.device.create_lsl_EEG(channels)
     pytest.device._set_n_samples(2)
+    mocker.patch.object(pytest.device._bitalino, 'start')
     mocker.patch.object(pytest.device._bitalino, 'read')
     pytest.device._bitalino.read.return_value = [[0,1,69], [0,1,70]]
     sampling_rate = pytest.device.get_sampling_rate()
@@ -51,8 +55,9 @@ def test_stream(capsys, mocker):
         with capsys.disabled():
             print(i)
         sample, timestamp = inlet.pull_sample()
+        now = time.time()
         with capsys.disabled():
-            print(timestamp)
+            print(now - timestamp)
         assert sample[1] != old_sample[1]
         assert sample[1] in [69, 70]
         assert timestamp == old_timestamp + 1/sampling_rate
