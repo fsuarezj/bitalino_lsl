d088611ca4ea724da94fdc8b56a0b1e36be160d2
Fernando Suárez <fsuarezj@gmail.com>
2018-10-06 22:37:07 +0200

    Minor changes

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="bitalino-lsl",
+    name="bitalino_lsl",
     version="0.0.3",
     author="Fernando Suarez Jimenez",
     author_email="fsuarezj@gmail.com",
